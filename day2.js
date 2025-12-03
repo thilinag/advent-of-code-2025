3b6cda276a027a31f2053e1d45c3ee858284cd75
Thilina Gunasekara <thilina.gunasekara@gmail.com>
2025-12-04 08:41:22 +1030

    add solution comment

--- day2.js
+++ day2.js
@@ -1,68 +1,79 @@
 const part1Data = {
-  sample: `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+    sample: `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
 1698522-1698528,446443-446449,38593856-38593862,565653-565659,
 824824821-824824827,2121212118-2121212124`,
-  answer: 1227775554,
+    answer: 1227775554,
 };
 
 const part2Data = {
-  sample: part1Data.sample,
-  answer: 4174379265,
+    sample: part1Data.sample,
+    answer: 4174379265,
 };
 
 const sampleData = [part1Data, part2Data];
 const isBrowser = typeof window !== "undefined";
 
 const getData = (part) => {
-  const input = isBrowser
-    ? document.body.innerText.trim()
-    : sampleData[part - 1].sample;
-  return input.split(",");
+    const input = isBrowser
+        ? document.body.innerText.trim()
+        : sampleData[part - 1].sample;
+    return input.split(",");
 };
 
 const part1 = () => {
-  const data = getData(1);
+    const data = getData(1);
 
-  const invalid = [];
+    /*
+     * Cut he number in half and check if both halves are equal
+     */
 
-  data.forEach((line) => {
-    const [min, max] = line.split("-").map(Number);
-    let current = min;
-    while (current <= max) {
-      const currentStr = String(current);
-      current++;
-      if (currentStr.length % 2 !== 0) continue;
-      const mid = currentStr.length / 2;
-      const left = currentStr.slice(0, mid);
-      const right = currentStr.slice(mid);
+    const invalid = [];
 
-      if (left === right) {
-        invalid.push(Number(currentStr));
-      }
-    }
-  });
+    data.forEach((line) => {
+        const [min, max] = line.split("-").map(Number);
+        let current = min;
+        while (current <= max) {
+            const currentStr = String(current);
+            current++;
+            if (currentStr.length % 2 !== 0) continue;
+            const mid = currentStr.length / 2;
+            const left = currentStr.slice(0, mid);
+            const right = currentStr.slice(mid);
 
-  return invalid.reduce((a, b) => a + b, 0);
+            if (left === right) {
+                invalid.push(Number(currentStr));
+            }
+        }
+    });
+
+    return invalid.reduce((a, b) => a + b, 0);
 };
 
 const part2 = () => {
-  const data = getData(2);
+    const data = getData(2);
 
-  const invalid = [];
+    /*
+     * Similar to part 1, but check for any group of repeated digits
+     * via regex capturing groups.
+     * For example, "1212" matches with "12" as the captured group,
+     * and "3333" matches with "3" as the captured group.
+     */
 
-  data.forEach((line) => {
-    const [min, max] = line.split("-").map(Number);
-    let current = min;
-    while (current <= max) {
-      const currentStr = String(current);
-      current++;
-      if (/^(.+)\1+$/.test(currentStr)) {
-        invalid.push(Number(currentStr));
-      }
-    }
-  });
+    const invalid = [];
 
-  return invalid.reduce((a, b) => a + b, 0);
+    data.forEach((line) => {
+        const [min, max] = line.split("-").map(Number);
+        let current = min;
+        while (current <= max) {
+            const currentStr = String(current);
+            current++;
+            if (/^(.+)\1+$/.test(currentStr)) {
+                invalid.push(Number(currentStr));
+            }
+        }
+    });
+
+    return invalid.reduce((a, b) => a + b, 0);
 };
 
 console.time("part1");
@@ -70,17 +81,17 @@ const part1Answer = part1();
 console.log({ part1: part1Answer });
 console.timeEnd("part1");
 if (!isBrowser)
-  console.assert(
-    part1Answer === part1Data.answer,
-    `${part1Data.answer} expected.`
-  );
+    console.assert(
+        part1Answer === part1Data.answer,
+        `${part1Data.answer} expected.`
+    );
 
 console.time("part2");
 const part2Answer = part2();
 console.log({ part2: part2Answer });
 console.timeEnd("part2");
 if (!isBrowser)
-  console.assert(
-    part2Answer === part2Data.answer,
-    `${part2Data.answer} expected.`
-  );
+    console.assert(
+        part2Answer === part2Data.answer,
+        `${part2Data.answer} expected.`
+    );
